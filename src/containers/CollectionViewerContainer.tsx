93e844e3faae9abd36f4afcdca6eafeac253f748
Alexander Rybakov <40580393+AlexRbkv@users.noreply.github.com>
2022-10-04 12:05:30 +0400

    SOROKA-127 W: added cards filter by organization (#17)
    SOROKA-127 W: added cards filter by organization
    SOROKA-127: filter cards by id without m+s org

--- src/containers/CollectionViewerContainer.tsx
+++ src/containers/CollectionViewerContainer.tsx
@@ -1,29 +1,59 @@
 import { useEffect, useState } from "react";
-import { CardsAPI, CardData } from "../api/cards";
+import { CardsAPI, CardData, Property } from "../api/cards";
 import CollectionViewer from "../components/CollectionViewer";
 
+const getPatchedCards = (cardsList: CardData[], propertiesList: Property[]) => {
+  // filling CardData properties names from all properties list array
+  const patched = cardsList.map((card) => ({
+    ...card,
+    propertiesList: card.propertiesList.map((cardProperty) => ({
+      ...cardProperty,
+      propertyName: propertiesList.find(({ propertyId }) => propertyId === cardProperty.propertyId)?.name || ""
+    }))
+  }));
+  const patchedWithoutMS = patched.filter((item) => item.organizationId !== 1);
+  return patchedWithoutMS;
+};
+
 const CollectionViewerContainer = () => {
+  const [filterValue, setFilterValue] = useState<number>(0);
+  const [organizations, setOrganizations] = useState<{ id: number; name: string }[]>([]);
   const [cardsList, setCardsList] = useState<CardData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    CardsAPI.getCardsProperties().then((propertiesList) =>
-      CardsAPI.getCardsByFirstOrganization().then((cardsList) => {
-        // filling CardData properties names from all properties list array
-        const patched = cardsList.map((card) => ({
-          ...card,
-          propertiesList: card.propertiesList.map((cardProperty) => ({
-            ...cardProperty,
-            propertyName: propertiesList.find(({ propertyId }) => propertyId === cardProperty.propertyId)?.name || ""
-          }))
-        }));
-        setIsLoading(false);
-        setCardsList(patched);
-      })
-    );
+    let orgMass: { id: number; name: string }[] = [];
+    CardsAPI.getOrganizations().then((res) => {
+      res.forEach((item, index) => {
+        if (item.id !== 1)
+          orgMass[index] = {
+            id: item.id,
+            name: item.name
+          };
+      });
+      setOrganizations(orgMass);
+    });
   }, []);
 
-  return <CollectionViewer cards={cardsList} isLoading={isLoading} />;
+  useEffect(() => {
+    CardsAPI.getCardsProperties().then((propertiesList) => {
+      if (filterValue === 0) {
+        CardsAPI.getCardsList().then((cardsList) => {
+          setIsLoading(false);
+          setCardsList(getPatchedCards(cardsList, propertiesList));
+        });
+      } else {
+        CardsAPI.getCardsByOrganizationId(filterValue).then((cardsList) => {
+          setIsLoading(false);
+          setCardsList(getPatchedCards(cardsList, propertiesList));
+        });
+      }
+    });
+  }, [filterValue]);
+
+  return (
+    <CollectionViewer cards={cardsList} organizations={organizations} isLoading={isLoading} setFilterValue={setFilterValue} />
+  );
 };
 
 export default CollectionViewerContainer;
