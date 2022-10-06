52d0655f1526a14f8457c7eabcd1d0b412b94fd5
Alexander Botenkov <boatincow@gmail.com>
2022-10-06 22:19:22 +0300

    SOROKA-141: Strings fix for wunderkammer

--- src/components/TextPropertyParser.tsx
+++ src/components/TextPropertyParser.tsx
@@ -2,7 +2,7 @@ import { PROPERTIES_NAMES } from "../utils/constants";
 import { PropertiesParserProps } from "./PropertiesParser";
 
 const TextPropertyParser = ({ property }: PropertiesParserProps) => {
-  return <p>{`${PROPERTIES_NAMES[property.propertyName]}: ${JSON.parse(property.data).value}`}</p>;
+  return <p>{`${PROPERTIES_NAMES[property.propertyName]}: ${JSON.parse(property.data)}`}</p>;
 };
 
 export default TextPropertyParser;
