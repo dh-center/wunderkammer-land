7094f06fd7b3b8ed91328fb6284080a77d88a202
Alexander Rybakov <40580393+AlexRbkv@users.noreply.github.com>
2022-10-06 14:49:02 +0400

    SOROKA-137: added typical view of map and collection pages (#20)
    SOROKA-137: added typical view of map and collection pages, common layout
    
    Co-authored-by: Alexander Botenkov <boatincow@gmail.com>

--- src/pages/CollectionDetailed.tsx
+++ src/pages/CollectionDetailed.tsx
@@ -1,5 +1,10 @@
+import PageLayout from "../components/PageLayout";
 import CollectionViewerContainer from "../containers/CollectionViewerContainer";
 
-const CollectionDetailed = () => <CollectionViewerContainer />;
+const CollectionDetailed = () => (
+  <PageLayout breadcrumbs={[{ label: "Коллекция", link: "/collection" }]}>
+    <CollectionViewerContainer />
+  </PageLayout>
+);
 
 export default CollectionDetailed;
